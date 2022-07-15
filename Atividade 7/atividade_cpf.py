b55d0adcc5c875f9c897cb564d30708ec229c28f
Brunu <comachiobelobruno@protonmail.com>
2022-07-14 21:54:12 -0400

    Progresso na atividade 7

--- Atividade 7/atividade_cpf.py
+++ Atividade 7/atividade_cpf.py
@@ -1,35 +1,47 @@
 import numpy as np
 
-cpf_9 = str(input("Digite os 9 primeiros digitos do CPF:"))
+# recebe os 9 digitos iniciais
+cpf_9 = str(input("Digite os 9 primeiros digitos do CPF: "))
 
+# recebe os 2 digitos finais de controle
+cpf_controle = str(input("Digite os (2) digitos de controle: "))
+
+# separa os numeros do cpf_9 em uma lista
 lista_cpf = [int(d) for d in str(cpf_9)]
+
+# cria uma nova_lista e preenche com 0
 nova_lista = np.zeros(9)
-
-
 multiplicado = 0
 digitu_1 = 0
 
+
+# -----------------------------
+# -----geração do 1 digito-----
+
+# multiplicacao de cada digito
 for i in range(9):
     nova_lista[i] = lista_cpf[i] * (10 - i)
     multiplicado += nova_lista[i]
 
+# modulo 11 para obter o resto
 restu = multiplicado % 11
 
-print(nova_lista)
-print(multiplicado)
-print(restu)
-
+# decide qual vai ser o primeiro digito baseado no resto obtido
 if (restu >= 2):
     digitu_1 = 11 - restu
 elif ((restu == 1) or (restu == 0)):
     digitu_1 = 0
 
-print(digitu_1)
-
+# adiciona o primeiro digito de controle na lista_cpf
+# adiciona esse digito para uma variavel propria para o digito de controle
 lista_cpf.append(int(digitu_1))
-print(lista_cpf)
+digitos_controle = str(int(digitu_1))
 
-# digito 2
+
+# -----------------------------
+# -----geração do 2 digito-----
+
+# mesmo procedimento com adesão do primeiro digito de controle
 
 digitu_2 = 0
 multiplicado_2 = 0
@@ -46,12 +58,9 @@ if (restu_2 >= 2):
 else:
     digitu_2 = 0
 
+# adiciona o segundo digito de controle na lista_cpf
 lista_cpf.append(int(digitu_2))
-print(lista_cpf)
+digitos_controle += str(int(digitu_2))
 
-listona_cpf = ''
 
-for i in range(11):
-    listona_cpf += str(lista_cpf[i])
-
-print(listona_cpf)
+# olha se o digitos_controle informado é valido, comparando com o obtido
