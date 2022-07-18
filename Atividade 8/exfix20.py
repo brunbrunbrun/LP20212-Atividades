a4982b69ea6321237295c60a0909d371651f96de
Brunu <comachiobelobruno@protonmail.com>
2022-07-18 05:11:27 -0400

    atividade 8 com coisinhas do pari

--- Atividade 8/exfix20.py
+++ Atividade 8/exfix20.py
@@ -1,1 +1,21 @@
-# até dia 20
+# import numpy as np
+# ^ sem uso ainda
+
+lista_altura_M = [] * 4
+lista_altura_F = [] * 4
+i = 0
+
+while (i < 4):
+    print("---------------")
+    altura = float(input("Digite a altura do aluno: "))
+    sexo = str(input("Qual o sexo do aluno? (M/F): "))
+
+    if(sexo == 'M'):
+        lista_altura_M = [altura]
+    else:
+        lista_altura_F = [altura]
+
+    i += 1
+
+print(lista_altura_M)
+print(lista_altura_F)
