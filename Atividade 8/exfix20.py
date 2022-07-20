c1b1ca19eb89c82797f3215043e343d063b22e73
Brunu <comachiobelobruno@protonmail.com>
2022-07-20 00:26:00 -0400

    hide's 8

--- Atividade 8/exfix20.py
+++ Atividade 8/exfix20.py
@@ -1,22 +1,49 @@
-# import numpy as np
-# ^ ainda não usada no codigo
+import numpy as np
 
-lista_altura_M = [] * 4
-# não entendi pq desse * 4, é pra delimitar o tamanho maximo da lista?
-lista_altura_F = [] * 4
-i = 0
+# define a quantidade de dados a serem lidos
+dados = 20
 
-while (i < 4):
-    print("---------------")
-    altura = float(input("Digite a altura do aluno: "))
-    sexo = str(input("Qual o sexo do aluno? (M/F): "))
+Alturas_Lista = []
+Alturas_Lista_Mulheres = []
+listaNotasMulheres = []
+listaNotasHomens = []
+dicionarioAnosNascimentos = {}
 
-    if(sexo == 'M'):
-        lista_altura_M.append(altura)
+
+for i in range(dados):
+    altura = float(input('Altura: '))
+    nota = float(input('Nota: '))
+    anoNascimento = int(input('Ano de nascimento: '))
+    sexo = input('Código do sexo ("M" ou "F"): ')
+    Alturas_Lista.append(altura)
+    if sexo == 'F':
+        Alturas_Lista_Mulheres.append(altura)
+        listaNotasMulheres.append(nota)
     else:
-        lista_altura_F.append(altura)
+        listaNotasHomens.append(nota)
+    if anoNascimento in dicionarioAnosNascimentos:
+        dicionarioAnosNascimentos[anoNascimento] += 1
+    else:
+        dicionarioAnosNascimentos[anoNascimento] = 1
 
-    i += 1
+vetorAlturas = np.array(Alturas_Lista)
+mediaAlturasMulheres = (np.array(Alturas_Lista_Mulheres)).mean()
+quantidadeMulheres = 0
+for altura in Alturas_Lista_Mulheres:
+    if altura > mediaAlturasMulheres:
+        quantidadeMulheres += 1
+mediaNotasMulheres = (np.array(listaNotasMulheres)).mean()
+quantidadeHomens = 0
+for nota in listaNotasHomens:
+    if nota < mediaNotasMulheres:
+        quantidadeHomens += 1
 
-print(lista_altura_M)
-print(lista_altura_F)
+print(f'\nMaior altura da turma: {vetorAlturas.max()}')
+print(f'Menor altura da turma: {vetorAlturas.min()}')
+print(f'Quantidade de mulheres com altura acima da média\
+        das alturas das mulheres: {quantidadeMulheres}')
+print(f'Quantidade de homens com nota inferior a média\
+        das notas das mulheres: {quantidadeHomens}')
+print('Percentual de pessoas que nasceram em cada ano inserido: ')
+for ano in dicionarioAnosNascimentos:
+    print(f'{ano} - {(dicionarioAnosNascimentos[ano] * 100 / dados):.2f}%')
